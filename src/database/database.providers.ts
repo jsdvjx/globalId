a061a8ba42860a9ac34732230ad53834a93787f7
jsdvjx <841207@gmail.com>
2018-11-17 00:17:27 +0800

    test

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,11 +5,11 @@ export const databaseProviders = [
         provide: 'DbConnectionToken',
         useFactory: async () => await createConnection({
             type: 'mysql',
-            host: 'localhost',
+            host: '35.194.210.98',
             port: 3306,
             username: 'root',
             password: '29915815',
-            database: 'global_id',
+            database: 'globalId',
             entities: [
                 __dirname + '/../**/*.entity{.ts,.js}',
             ],
